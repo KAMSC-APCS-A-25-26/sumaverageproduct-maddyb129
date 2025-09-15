ee232852a7dcf0ce77ba9ca53dd151c07015529a
maddyb129 <maddyball814@gmail.com>
2025-09-15 13:44:46 -0400

    Update SumProductAverage.java

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -13,10 +13,42 @@
  * 5. After processing all n sets, the program ends.
  */
 
+
+/**
+ * NAME                 : Madilyn Ball
+ * GROUP                : APCS-A
+ * LAST MODIFIED        : 08 September 2025
+ * PROBLEM ID           : Lewis 2.3 Arithmetic Problem
+ * PROBLEM DESCRIPTION  : An application that reads two floating point numbers
+ *                        and prints their sum, difference, and product.
+ * SOURCES I USED       : Lewis Java Software Solutions 3rd Edition
+ * PEOPLE I HELPED      :
+ * PEOPLE WHO HELPED ME : Mr Houtrouw, Mr Afsal
+ */
 import java.util.Scanner;
 
-public class SumProductAverage {
+public class SumProductAverage
+{
     public static void main(String[] args) {
-        // TODO
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Madilyn Ball\nAPCS-A\nLewis Problem 2.3\n");
+        System.out.print("\nEnter the number of times you want to run the program: ");
+        int numRuns = sc.nextInt();
+        for (int j = 0; j < numRuns; j++){
+            System.out.print("Enter the first number: ");
+            double num1 = sc.nextDouble();
+
+            System.out.print("Enter the second number: ");
+            double num2 = sc.nextDouble();
+
+            double sum = num1 + num2;
+            double product = num1 * num2;
+            double average = sum / 3.0;
+            System.out.println("Sum: " + sum);
+            System.out.println("Product: " + product);
+            System.out.println("Average: " + average);
+            System.out.println();
+        }
+        sc.close();
     }
 }
